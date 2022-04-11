8b560f087a8a5f68d240b229e0e6f56e901f5cc0
Adrian-enlaptop <adrianmetal1011@gmail.com>
2022-04-11 18:26:37 -0500

    POST EN MONGODB

--- index.js
+++ index.js
@@ -5,35 +5,32 @@ const port = 3000
 const path = require('path');
 const mongoose = require('mongoose')
 require('dotenv').config(); //seteamos el donenv
+const morgan = require('morgan');
+const indexRoutes = require('./routes/index');
+const Task = require('./views/models/task');
+const router = express.Router();
 
 app.set('view engine', 'ejs')
 app.set('views',path.join(__dirname, '/views'))
 
+
 app.use(express.urlencoded({extended: false})); //JSON
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"))
 
 
 //INICIO DE MONGODB (CONEXION)
 const MONGODBHOST = process.env.MONGODBHOST;
 const MONGODBDATABASE = process.env.MONGODBDATABASE;
 
-const MONGODB_URI = `mongodb://${MONGODBHOST}/${MONGODBDATABASE}`;
-
-mongoose.connect(MONGODB_URI, function(err){ 
-    if (err){
-        throw err;
-    }else{
-        console.log(`BIENVENIDO A LA BASE DE ${MONGODB_URI}`)
-    }
-}) 
-
-.then(db => console.log('Ya jalo la base de datos, CONECTADA'))
+mongoose.connect(`mongodb://${MONGODBHOST}/${MONGODBDATABASE}`)
+.then(db => console.log('Db Connectando'))
 .catch(err => console.log(err));
 
 //FIN DE MONGODB (CONEXION)
 
 console.log(process.env.MONGODBDATABASE);// mensaje en la consola
 
-app.use(express.static(__dirname + "/public"))
 
 /*SETE DE RUTAS*/
 app.get('/', (req, res) => {
@@ -48,14 +45,24 @@ app.get('/home', (req, res) => {
 app.get('/vista2', (req, res) => {
   res.render('vista2')
 })
-app.get('/vista3', (req, res) => {
-  res.render('vista3')
+app.get('/vista3', async (req, res) => {
+  const tasks = await Task.find();
+  console.log(tasks);
+  res.render('vista3', {
+    tasks
+  })
 })
 app.get('/vista4', (req, res) => {
   res.render('vista4')
 })
 app.get('/vista5', (req, res) => {
   res.render('vista5')
+})
+
+app.post('/add', async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.send('recibido')
 })
 
 /*FINAL DE RUTAS*/
