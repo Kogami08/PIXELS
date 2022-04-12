85c79838e45d3812823e56ea1a7155ab86887747
Adrian-enlaptop <adrianmetal1011@gmail.com>
2022-04-11 19:00:10 -0500

    BOTON DELETE (Sin funcionar)

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('dotenv').config(); //seteamos el donenv
 const morgan = require('morgan');
 const indexRoutes = require('./routes/index');
 const Task = require('./views/models/task');
+const req = require('express/lib/request');
 const router = express.Router();
 
 app.set('view engine', 'ejs')
@@ -35,41 +36,46 @@ console.log(process.env.MONGODBDATABASE);// mensaje en la consola
 /*SETE DE RUTAS*/
 app.get('/', (req, res) => {
   res.render('index')
-})
+});
 app.get('/login', (req, res) => {
   res.render('login')
-})
+});
 app.get('/home', (req, res) => {
   res.render('home')
-})
+});
 app.get('/vista2', (req, res) => {
   res.render('vista2')
-})
+});
 app.get('/vista3', async (req, res) => {
   const tasks = await Task.find();
   console.log(tasks);
   res.render('vista3', {
     tasks
   })
-})
+});
 app.get('/vista4', (req, res) => {
   res.render('vista4')
-})
+});
 app.get('/vista5', (req, res) => {
   res.render('vista5')
-})
+});
 
 app.post('/add', async (req, res) => {
   const task = new Task(req.body);
   await task.save();
-  res.send('recibido')
+  res.redirect('/vista3');
+});
+
+router.get('/delete/:id', (req, res) => {
+console.log(req.params)
+res.send('recibidoooo!');
 })
 
-/*FINAL DE RUTAS*/
+/*FINAL DE RUTAS
 
 app.use((req,res,next) => {
     res.status(404).sendFile(__dirname + '/public/404.html' )
-  }) 
+  }) */
 
 app.listen(port, () => {
     console.log(`la aplicacion esta funcionando en:http://localhost:${port}`)
